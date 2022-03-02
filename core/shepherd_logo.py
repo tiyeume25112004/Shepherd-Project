d8bb1e1e4e0c7559015204244b09025e58baebf9
Shepherd <81541045+LostShepherdUK@users.noreply.github.com>
2022-03-02 21:31:51 +0000

    Bug fix.
    Bug fix.

--- core/shepherd_logo.py
+++ core/shepherd_logo.py
@@ -8,7 +8,6 @@ import os # Operating System functions.
 import subprocess # So we can call other scripts!
 import re # Regular expression, because we all love that... right?
 import argparse # Google said I needed this.
-import core.shepherd_logo as logo # My logo, of course.
 import core.colors as colors # We all love a splash of color.
 import core.mods as mods # So we can let people modify it easier!
 
